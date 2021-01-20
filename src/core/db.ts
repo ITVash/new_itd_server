bcc3981c95dfaaa21c5236d83b3fc8d9f59931d2
Vash <vash_dn@mail.ru>
2021-01-20 22:11:57 +0300

    Update

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 //mongodb+srv://admin:dtkjcbgtl2020@dontrc.4b9is.gcp.mongodb.net/itdcompany?retryWrites=true&w=majority
-const LINK: string = process.env.LINK_DB || "http://localhost:27017/dtrc"
+const LINK: string = process.env.LINK_DB || "http://localhost:27017/itdcompany"
 mongoose.connect(LINK, {
 	useCreateIndex: true,
 	useNewUrlParser: true,
